0f5fe778d722abc4af2421bbd72c39d08d620446
Wendall Gee <coolgesus@gmail.com>
2014-12-08 19:21:37 -0800

    pushing latest

--- 04_Collections_APIs/starter_code/qf_fizz_buzz.rb
+++ 04_Collections_APIs/starter_code/qf_fizz_buzz.rb
@@ -5,6 +5,21 @@ end
 
 
 def fizzbuzz(number)
+
+	if number % 5 == 0 && number % 3 == 0
+		return "Fizzbuzz"
+	
+	elsif number % 3 == 0
+		return "Fizz"
+
+	elsif number % 5 == 0
+		return "Buzz"
+	
+	else 
+		return number
+
+	end
+		
 end
 
 
